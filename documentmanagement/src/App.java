54e430cdc9e5e5225b85b341559d3db6c819c79d
NG291 <you@example.com>
2024-07-11 15:23:36 +0700

    capnhat

--- documentmanagement/src/App.java
+++ documentmanagement/src/App.java
@@ -1,4 +1,5 @@
 import manager.Manager;
+import menu.Menu;
 import menu.MenuCustomers;
 import menu.MenuDocument;
 
@@ -6,32 +7,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Manager manager = new Manager();
-        try {
-            while (true) {
-            System.out.println(" Document system management  ");
-            System.out.println("1:Document Management");
-            System.out.println("2:Manage borrowers");
-            System.out.println("0:Exit the program");
-                int chocie = Integer.parseInt(scanner.nextLine());
-                if (chocie == 0) {
-                    break;
-                }
-                switch (chocie) {
-                    case 1:
-                        MenuDocument.menuSelect2(scanner, manager);
-                        break;
-                    case 2:
-                        MenuCustomers.menuSelect1(scanner, manager);
-                        break;
-                    default:
-                        System.out.println("Retype!");
-                        break;
-                }
-            }
-        }catch (Exception e){
-            System.out.println(e.getMessage());
-        }
+        Menu menu =new Menu();
+        menu.Display();
     }
 }
---
+++ documentmanagement/src/menu/Menu.java
@@ -0,0 +1,38 @@
+package menu;
+
+import manager.Manager;
+
+import java.util.Scanner;
+
+public class Menu {
+    public void Display() {
+        Scanner scanner = new Scanner(System.in);
+        Manager manager = new Manager();
+        try {
+            while (true) {
+                System.out.println(" Document system management  ");
+                System.out.println("1:Document Management");
+                System.out.println("2:Manage borrowers");
+                System.out.println("0:Exit the program");
+                int chocie = Integer.parseInt(scanner.nextLine());
+                if (chocie == 0) {
+                    break;
+                }
+                switch (chocie) {
+                    case 1:
+                        MenuDocument.menuSelect2(scanner, manager);
+                        break;
+                    case 2:
+                        MenuCustomers.menuSelect1(scanner, manager);
+                        break;
+                    default:
+                        System.out.println("Retype!");
+                        break;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
